5b8dc0eff311027977bcdc19b161cc5bfc6ee715
Ashton Teng <yqteng@berkeley.edu>
2017-10-19 08:14:53 +0000

    removed except :index, so that resources :posts can work as convention

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resource :posts, except: [:index, :destroy]
+  resources :posts, except: [:destroy]
   #post "/posts/create", to: :create, controller: 'post'
   post "/posts/new", to:"posts#create"
   # You can have the root of your site routed with "root"
