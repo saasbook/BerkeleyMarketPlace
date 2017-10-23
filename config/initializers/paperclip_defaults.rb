dd7af221d65e54b125b7971ff2c542d8643ca1d2
Zhaoqin Ye <yezhaoqin@berkeley.edu>
2017-10-23 03:21:41 +0000

    use cedar-14

--- config/initializers/paperclip_defaults.rb
+++ config/initializers/paperclip_defaults.rb
@@ -1,10 +1,10 @@
-# module Paperclip
-#  class Processor
-# def convert(arguments = "", local_options = {})
-#  if (arguments =~ /-resize/)
-#   arguments.sub!("-resize", "-filter Lanczos -resize")
-#  end
-#  Paperclip.run('convert', arguments, local_options)
-# end
-#  end
-# end
+module Paperclip
+ class Processor
+def convert(arguments = "", local_options = {})
+ if (arguments =~ /-resize/)
+  arguments.sub!("-resize", "-filter Lanczos -resize")
+ end
+ Paperclip.run('convert', arguments, local_options)
+end
+ end
+end
