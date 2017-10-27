78b1cf3174f7580e8e82c6cd7284e28265fadb93
Zhaoqin Ye <yezhaoqin@berkeley.edu>
2017-10-27 20:51:17 +0000

    add login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception unless Rails.env.test?
   #protect_from_forgery with: :null_session
+  
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 
   def index 
     @posts = Post.get_all_valid_posts
--- config/routes.rb
+++ config/routes.rb
@@ -1,69 +1,20 @@
 Rails.application.routes.draw do
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-  resources :posts, except: [:destroy]
-  resources :users, except: [:destroy]
   
-  #post "/posts/create", to: :create, controller: 'post'
-  post "/posts/new", to:"posts#create"
-  post "/posts/create", to: "posts#create"
-  # You can have the root of your site routed with "root"
+  # root
   root 'application#index'
+
+  # resources
+  resources :posts, except: [:index, :destroy]
+  resources :users, except: [:destroy]
+  resources :sessions, only: [:create, :destroy]
   
+  # authentication
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get '/logout', to: 'sessions#destroy', as: 'logout'
+  
+  # ajax calls
   get "/filter" => 'application#filter', as: 'filter', format: 'js'
-  
   get "/update_form_subcategory" => 'posts#update_form_subcategory', as: 'update_form_subcategory', format: 'js'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  
 end
