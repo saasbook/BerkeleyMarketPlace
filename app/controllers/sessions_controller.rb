c9d3a96f238de59151518c98c03d71ca044ed406
Zhaoqin Ye <yezhaoqin@berkeley.edu>
2017-10-28 23:51:14 +0000

    add logger to see the issue in prod

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,11 @@ class SessionsController < ApplicationController
   def create
     begin
       user = User.from_omniauth(env["omniauth.auth"])
-      puts "set user_id in session to <%s>" % user.email
+      logger.debug "set user_id in session to <%s>" % user.email
       session[:user_id] = user.id
       redirect_to root_path
-    rescue
+    rescue => e
+      logger.error e
       flash[:notice] = "please login with @berkeley.edu email"
       redirect_to root_path
     end
