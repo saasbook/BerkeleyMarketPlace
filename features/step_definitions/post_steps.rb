3e57f59c093f20175c1b926ba663f275c4fc3505
Julie Deng <shuyindeng@berkeley.edu>
2017-11-02 18:40:28 +0000

    [test] add search tests

--- features/step_definitions/post_steps.rb
+++ features/step_definitions/post_steps.rb
@@ -21,7 +21,57 @@ When /^I wait (\d+) seconds?$/ do |seconds|
   sleep seconds.to_i
 end
 
+
+
+
 # Then /^I should have image in the field "(.*)"$/ do |field|
 #   file =  File.join(Rails.root, "public/images/post_default.png")
 #   assert file.exists()
 # end
+
+
+
+Then /^(?:|I )should not see any posts$/ do
+  page.should have_no_css('div.card')
+end
+
+Then /^(?:|I )should see all posts$/ do
+  Post.all.each do |post|
+    # byebug
+    if post.available == true 
+      if post.expire_time 
+        step %{I should see "#{post.title}"} unless post.expire_time.utc.strftime( "%H%M%S%N" ) < Time.now.utc.strftime( "%H%M%S%N" )
+      else
+        step %{I should see "#{post.title}"}
+      end
+    end
+  end
+end
+
+Then /^(?:|I )should see all posts with category "(.*)"$/ do |searched_category|
+  Post.all.each do |post|
+    if post.category == searched_category and post.available == true
+      if post.expire_time 
+        step %{I should see "#{post.title}"} unless post.expire_time.utc.strftime( "%H%M%S%N" ) < Time.now.utc.strftime( "%H%M%S%N" )
+      else
+        step %{I should see "#{post.title}"}
+      end
+    end
+  end
+end
+
+Then /^(?:|I )should see all posts with subcategory "(.*)"$/ do |searched_subcategory|
+  Post.all.each do |post|
+    if post.subcategory == searched_subcategory and post.available == true 
+      if post.expire_time 
+        step %{I should see "#{post.title}"} unless post.expire_time.utc.strftime( "%H%M%S%N" ) < Time.now.utc.strftime( "%H%M%S%N" )
+      else
+        step %{I should see "#{post.title}"}
+      end
+    end
+  end
+end
+
+
+
+
