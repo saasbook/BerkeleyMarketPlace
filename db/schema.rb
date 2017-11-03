794e9d0976c9ab1834c06e9e880291ca8397cb79
Zhaoqin Ye <yezhaoqin@berkeley.edu>
2017-11-03 22:42:00 +0000

    [fix] fix Gemfile pg version, fix search.feature keyword due to change in seed

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20171028212304) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.string   "price"
