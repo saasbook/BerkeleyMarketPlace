f31a66a834fa407eaddd964bd63d4566ce931498
Ashton Teng <yqteng@berkeley.edu>
2017-11-05 19:47:50 +0000

    [refactor] remove expire_time concept
    remove expire_time from post.rb
    remove expire_time from seeds
    generate migration to remove expire_time column

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,19 +15,19 @@ def generate_user_posts user
     item_posts = [
         {
             title: "iClicker %d for sale" % user.id, category: "item", subcategory: "electronics", price: 233.3,
-            available: true, release_time: random_past_time, expire_time: future_time,
+            available: true, release_time: random_past_time, 
         },
         {
             title: "Calculus %d Solution Manual" % user.id, category: "item", subcategory: "book", price: 233.3,
-            available: true, release_time: random_past_time, expire_time: future_time,
+            available: true, release_time: random_past_time, 
         },
         {
             title: "Linear Algebra %d Solution Manual" % user.id, category: "item", subcategory: "book", price: 233.3,
-            available: false, release_time: random_past_time, expire_time: future_time,
+            available: false, release_time: random_past_time, 
         },
         {
             title: "Old guitar, used for %d years" % user.id, category: "item", subcategory: "music related", price: 233.3,
-            available: true, release_time: random_past_time, expire_time: random_past_time,
+            available: true, release_time: random_past_time,
         },
         {
             title: "XBOX %d with 20 games bundle, just $233.3" % user.id, category: "item", subcategory: "electronics", price: 233.3,
@@ -43,26 +43,26 @@ def generate_user_posts user
         },
         {
             title: "Used king bed, IKEA NO.%d worst seller" % user.id, category: "item", subcategory: "furniture", price: 233.3,
-            available: true, release_time: random_past_time, expire_time: future_time,
+            available: true, release_time: random_past_time, 
         },
     ]
 
     job_posts = [
         {
             title: "%d machine learning expert needed" % user.id, category: "job", subcategory: "computer science", price: 233.3,
-            available: true, release_time: random_past_time, expire_time: future_time,
+            available: true, release_time: random_past_time, 
         },
         {
             title: "%d workers for house-moving, 20$/hour" % user.id, category: "job", subcategory: "daily life", price: 233.3,
-            available: true, release_time: random_past_time, expire_time: future_time,
+            available: true, release_time: random_past_time, 
         },
         {
             title: "Short-term %d Math tutor" % user.id, category: "job", subcategory: "math", price: 233.3,
-            available: false, release_time: random_past_time, expire_time: future_time,
+            available: false, release_time: random_past_time, 
         },
         {
             title: "%d pianoists for performance" % user.id, category: "job", subcategory: "performance", price: 233.3,
-            available: true, release_time: random_past_time, expire_time: random_past_time,
+            available: true, release_time: random_past_time,
         },
         {
             title: "Help me eat chicken in PUBG for %d times! Highly rewarded!!" % user.id, category: "job", subcategory: "daily life", price: 233.3,
@@ -70,11 +70,11 @@ def generate_user_posts user
         },
         {
             title: "Beg %d workers to clean the bathroom for $233.3/h" % user.id, category: "job", subcategory: "daily life", price: 233.3,
-            available: true, release_time: random_past_time, expire_time: future_time,
+            available: true, release_time: random_past_time, 
         },
         {
             title: "Taking notes for CS188 and CS189 for %d years" % user.id, category: "job", subcategory: "computer science", price: 233.3,
-            available: true, release_time: random_past_time, expire_time: future_time,
+            available: true, release_time: random_past_time, 
         },
         
     ]
@@ -82,31 +82,31 @@ def generate_user_posts user
     event_posts = [
         {
             title: "%d Concert tickets in SF" % user.id, category: "event", subcategory: "performance", price: 233.3,
-            available: true, release_time: random_past_time, expire_time: future_time,
+            available: true, release_time: random_past_time, 
         },
         {
             title: "%d Berkeley VS Stanford tickets" % user.id, category: "event", subcategory: "sports", price: 233.3,
-            available: true, release_time: random_past_time, expire_time: future_time,
+            available: true, release_time: random_past_time, 
         },
         {
             title: "%d hour Asian life matters speech in Sproul" % user.id, category: "event", subcategory: "speech", price: 233.3,
-            available: false, release_time: random_past_time, expire_time: future_time,
+            available: false, release_time: random_past_time, 
         },
         {
             title: "Private dinner with %d Google executive board" % user.id, category: "event", subcategory: "info session", price: 233.3,
-            available: true, release_time: random_past_time, expire_time: random_past_time,
+            available: true, release_time: random_past_time,
         },
         {
             title: "Berkeley MarketPlace NO.%d Info Session" % user.id, category: "event", subcategory: "info session", price: 233.3,
-            available: true, release_time: random_past_time, expire_time: future_time,
+            available: true, release_time: random_past_time, 
         },
         {
             title: "Make Berkeley Great For %d Times, public speech in People's Park" % user.id, category: "event", subcategory: "speech", price: 233.3,
-            available: true, release_time: random_past_time, expire_time: future_time,
+            available: true, release_time: random_past_time, 
         },
         {
             title: "Date me on Valentine's day %d years later, 233.3/h rate" % user.id, category: "event", subcategory: "daily life", price: 233.3,
-            available: true, release_time: random_past_time, expire_time: future_time,
+            available: true, release_time: random_past_time, 
         },
     ]
     
